ce543563494d9bbeb036e29d2bd122d067f69d16
akbarsanihasan <akbarsanihasan.pro@gmail.com>
2026-08-29 19:18:21 +0700

    main

--- desktop/aur.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! yay --version &>/dev/null; then
-	sudo rm -rf /tmp/yay
-	git clone https://aur.archlinux.org/yay-bin.git /tmp/yay
-
-	makepkg -sri --noconfirm -D /tmp/yay
-fi
--- extras/intel-graphics.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-clear
-
-sudo pacman -S --noconfirm --needed mesa \
-	vulkan-intel \
-	intel-media-driver \
-	libva-intel-driver \
-	intel-gpu-tools
